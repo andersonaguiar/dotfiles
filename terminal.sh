88c8a4abea150728266402f229018bdf3f894ed9
Anderson Aguiar <revsoft10@gmail.com>
2020-06-28 18:23:57 +1000

    Update

--- terminal.sh
+++ terminal.sh
@@ -3,3 +3,11 @@ brew install zsh
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
 nvm install node
 curl -L git.io/antigen > ~/antigen.zsh
+brew install z
+grep -q z\.sh ~/.zshrc || echo ". /usr/local/etc/profile.d/z.sh" >> ~/.zshrc
+brew install fd # https://github.com/sharkdp/fd
+brew install htop
+brew install glances
+brew install ctop
+brew install lazydocker
+echo "alias lzd='lazydocker'" >> ~/.zshrc
